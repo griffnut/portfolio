5f6d8fea8b39f95044ccc263948f6206e51bf3f5
unknown <lily.li0597@gmail.com>
2019-08-19 13:02:59 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Largest Heading
+## Add more '#' to make smaller
+
+**bold 1** && __bold 2__
+*italic 1* && _italic 2_
+***bold and italic***
+~~strikethrough~~
+
+> quote
+
+this is `code`
+this is ```for multi-line code```
+
+[link name](https://github.com/)
+
+[relative link](path/to/file)
+
+Lists
+- hello
+- world
+
+1. hello
+2. world
+
+Nested Lists
+1. hello
+  - world
+
+- [x] complete task
+- [ ] incomplete task
+
+ignore markdown:
+add \
+\* hello? \*
---
+++ server/index.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 1337;
+
+// request things like 'bundle.js'
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+// logging middleware
+app.use(morgan('dev'));
+
+// for using req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('*', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// 404s || user errors
+app.use(function (req, res, next) {
+  const err = new Error('Not found.');
+  err.status = 404;
+  next(err);
+});
+
+// 500s || our errors
+app.use(function (err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+app.listen(port, function () {
+  console.log(`listening on port ${port}`);
+});
